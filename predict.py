16aaf64cffb898f0e2868ac22e159ad78e52e899
Chaitanya Babu <132441058+dcb5803@users.noreply.github.com>
2025-10-15 19:32:03 +0530

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,14 @@
+import mlflow.sklearn
+import pandas as pd
+
+model = mlflow.sklearn.load_model("models:/credit_model/1")
+
+# Example input
+sample = pd.DataFrame({
+    "Age": [30],
+    "Income": [55000],
+    "CreditScore": [680]
+})
+
+prediction = model.predict(sample)
+print("Approved" if prediction[0] == 1 else "Rejected")

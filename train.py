67bf3fa3c012c839c8cec0f85646675a990b330c
Chaitanya Babu <132441058+dcb5803@users.noreply.github.com>
2025-10-15 19:42:49 +0530

    Update train.py

--- train.py
+++ train.py
@@ -26,4 +26,5 @@ with mlflow.start_run():
 
     mlflow.log_metric("accuracy", acc)
     mlflow.sklearn.log_model(model, "model")
-    print(f"Model trained and logged with accuracy: {acc}")
+    print(f"✅ Model trained and logged with accuracy: {acc}")
+
